6f643042f837eab15d87bf95914e3e1778d9dbb2
omarali18 <omarali1814000@gmail.com>
2021-10-12 21:57:33 +0600

    features added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import "./Priduct.css"
 import Rating from 'react-rating';
 
 const Product = (props) => {
+    // const [feature, setFeature] = useState({})
     const { img, name, seller, price, stock, star } = props.product
     const addToCartIcon = <FontAwesomeIcon icon={faShoppingCart} />
 
-    console.log(props.product);
+    const features = props.product.features
+    if (features) {
+        for (const feature of features) {
+            // console.log(feature.description);
+            // setFeature(feature)
+        }
+    }
+
+    // console.log(feature);
     return (
         <div className='product'>
             <div>
@@ -23,8 +32,8 @@ const Product = (props) => {
                         <p><small>only {stock} left in stock - order soon</small></p>
                         <button onClick={() => props.addBtnHandler(props.product)}>{addToCartIcon} add to cart</button>
                     </div>
-                    <div>
-                        <div>
+                    <div className="Features-container">
+                        <div className="rating-container">
                             <Rating
                                 className="rating"
                                 initialRating={star}
@@ -33,11 +42,19 @@ const Product = (props) => {
                                 readonly
                             ></Rating>
                         </div>
-                        <h3>Features</h3>
-                        <p>ekhan theke suru</p>
-                        <ul>
-                            {/* <li>{description}: {value}</li> */}
-                        </ul>
+                        <div className="Features">
+                            <h3>Features</h3>
+                            {
+                                features.map(feature => <table>
+                                    <tr>
+                                        <td>{feature.description} :- </td>
+                                        <td>{feature.value}</td>
+                                    </tr>
+                                </table>
+                                )
+                            }
+
+                        </div>
                     </div>
                 </div>
             </div>
